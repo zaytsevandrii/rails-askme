cbe4da34505030c6a7130fd81d62dd515c3277c9
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-01-14 10:11:38 +0000

    add links for all questions and pages questions all and question id

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,4 +15,18 @@ class QuestionsController < ApplicationController
             body:params[:question][:body],
             user_id:params[:question][:user_id]
         )
+    end
+
+    def destroy
+        @question = Question.find(params[:id])
+        @question.destroy
+    end
+
+    def show
+        @question=Question.find(params[:id])
+    end
+
+    def index
+        @questions=Question.all
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   post '/questions', to: 'questions#create'
   patch '/questions/:id', to: 'questions#update'
+  delete '/questions/:id', to: 'questions#destroy'
+  resources :questions
 end
