b838a42d0a4d0580d0d43ce141151a5c07f6c82d
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-01-14 17:08:39 +0000

    create form for edit,delete,read

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,32 +1,45 @@
 class QuestionsController < ApplicationController
-    skip_before_action :verify_authenticity_token
+    before_action :set_question, only: %i[update show destroy edit]
     def create
-        Question.create(
-            body:params[:question][:body],
-            user_id:params[:question][:user_id]
-        )
+        question = Question.create(question_params)
 
-        render text: 'Your request was successful'
+        redirect_to question_path(question)
     end
 
     def update
-        @question = Question.find(params[:id])
         @question.update(
             body:params[:question][:body],
             user_id:params[:question][:user_id]
         )
+        redirect_to question_path(@question)
     end
 
     def destroy
-        @question = Question.find(params[:id])
         @question.destroy
+        redirect_to questions_path
     end
 
     def show
-        @question=Question.find(params[:id])
     end
 
     def index
         @questions=Question.all
+    end
+
+    def new
+        @question=Question.new
+    end
+
+    def edit
+    end
+
+    private
+
+    def question_params
+        params.require(:question).permit(:body,:user_id)
+    end
+
+    def set_question
+        @question = Question.find(params[:id])
     end
 end
