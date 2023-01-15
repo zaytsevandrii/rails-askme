30e3acfcfe425ff62d1fc69ed2397f37a5488b2b
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-01-15 16:53:17 +0000

    add flashes and helpers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
     before_action :set_question, only: %i[update show destroy edit]
     def create
         question = Question.create(question_params)
-
+        flash[:notice] = "Your question has been created"
         redirect_to question_path(question)
     end
 
@@ -11,18 +11,19 @@ class QuestionsController < ApplicationController
             body:params[:question][:body],
             user_id:params[:question][:user_id]
         )
-        redirect_to question_path(@question)
+        redirect_to question_path(@question), notice: 'Question has been updated'
     end
 
     def destroy
         @question.destroy
-        redirect_to questions_path
+        redirect_to questions_path, notice: 'Question has been deleted'
     end
 
     def show
     end
 
     def index
+        @question=Question.new
         @questions=Question.all
     end
 
