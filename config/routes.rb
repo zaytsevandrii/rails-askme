8d4db6c1cd11df562e46689e3e211b8d6e2b122d
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-01-16 09:46:53 +0000

    add model user and controller users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   root to: 'questions#index'
   post '/questions', to: 'questions#create'
   patch '/questions/:id', to: 'questions#update'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_13_140005) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_16_094100) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "nickname"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
